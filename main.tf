546375778bc860ef03e64afdbcdc9c36fc9e6988
m0nda <79662294+m0nda@users.noreply.github.com>
2021-02-25 14:57:43 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami                    = (var.server_os == "ubuntu") ? data.aws_ami.ubuntu.image_id : data.aws_ami.windows.image_id
+  instance_type          = "t2.micro"
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    "Identity"    = var.identity
+    "Name"        = "Student"
+    "Environment" = "Training"
+  }
+}
