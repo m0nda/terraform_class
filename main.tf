921158bb8deee2d25175ea512e97ec1e255bb331
m0nda <79662294+m0nda@users.noreply.github.com>
2021-02-25 15:06:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-  
-variable subnet_id {}
-variable vpc_security_group_ids {
-  type = list
-}
-# variable identity {}
-
-variable server_os {
-    type = string
-    description = "Server Operating System"
-    default = "ubuntu"
-}
-
 resource "aws_instance" "web" {
   ami                    = (var.server_os == "ubuntu") ? data.aws_ami.ubuntu.image_id : data.aws_ami.windows.image_id
   instance_type          = "t2.micro"
