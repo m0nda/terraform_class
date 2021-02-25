ce6843db305de8755a84880f98ee760c15d265a4
m0nda <79662294+m0nda@users.noreply.github.com>
2021-02-25 16:09:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,11 @@
-variable subnet_id {}
-variable region {}
-variable vpc_security_group_ids {
-  type = list
-}
-# variable identity {}
-
-variable server_os {
-    type = string
-    description = "Server Operating System"
-    default = "ubuntu"
-}
-
 resource "aws_instance" "web" {
   ami                    = (var.server_os == "ubuntu") ? data.aws_ami.ubuntu.image_id : data.aws_ami.windows.image_id
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
-  region                 = "us-east-1"
 
   tags = {
-#    "Identity"    = var.identity
+    "Identity"    = var.identity
     "Name"        = "Student"
     "Environment" = "Training"
   }
