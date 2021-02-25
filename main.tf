c78bc2fbea23a3385ed16e4421914ef82d6d2587
m0nda <79662294+m0nda@users.noreply.github.com>
2021-02-25 16:12:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
+  region                 = var.region
 
   tags = {
     "Identity"    = var.identity
