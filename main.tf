2eac5511f751086bbbb9edc65c4a16c6d77657ce
m0nda <79662294+m0nda@users.noreply.github.com>
2021-02-25 16:15:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@ variable vpc_security_group_ids {
   type = list
 }
 variable identity {}
-
+variable "region" {
+  default = "<REGION>"
+}
 variable server_os {
     type = string
     description = "Server Operating System"
