2719089383dfb3b1c12389b178182de7f2c1dfa3
m0nda <79662294+m0nda@users.noreply.github.com>
2021-02-25 14:58:14 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable subnet_id {}
+variable vpc_security_group_ids {
+  type = list
+}
+variable identity {}
+
+variable server_os {
+    type = string
+    description = "Server Operating System"
+    default = "ubuntu"
+}
